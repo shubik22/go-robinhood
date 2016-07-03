21a8ffaa5e6246c685bbe291bcdd9a9e8c0f6c9e
Bahniuk <code@getbraintree.com>
2016-07-02 21:16:13 -0700

    adding components and fixing pathing
    Former-commit-id: 82a7a2bb24043994809b104333e1378e685fc991

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 'use strict'
 
 const React = require('react');
+const ReactDOM = require('react-dom');
+const Page = require('./components/page');
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('app')
+);
---
+++ js/components/page.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const React = require('react');
+
+const Page = React.createClass({
+  render: () => {
+    return (
+      <div>Loaded from React</div>
+    );
+  }
+});
+
+module.exports = Page;
+
